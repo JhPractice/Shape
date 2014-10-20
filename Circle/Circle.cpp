e40a6e56821ff58aa3e197e0bf7034c5ebbb47dc
JihwanKimA <hihig@naver.com>
2014-10-20 16:56:57 +0900

    source delete

--- Circle/Circle.cpp
+++ Circle/Circle.cpp
@@ -3,15 +3,10 @@
 #include "Processor.h"
 #include "Renderer.h"
 
-
 int main()
 {
-	//HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	//SetConsoleTextAttribute(hConsole, 1);
-
 	Renderer renderer;
 	Processor* processor = Processor::GetInstance();
-	renderer.Render();
 
 	while (processor->Process())
 	{
